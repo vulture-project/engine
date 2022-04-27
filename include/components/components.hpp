43733ccb05fb29e97b1f563b5726e1b118216b02
vasez <vasezs@inbox.ru>
2022-04-28 01:51:12 +0300

    added transform component and mesh component

---
+++ include/components/components.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "ECS/component.h"
+#include "renderer/core.hpp"
+#include "renderer/scene.hpp"
+
+class TransformComponent : ecs::Component<TransformComponent> {
+  public:
+    TransformComponent(const glm::vec3& translation) : translation{translation} {}
+    TransformComponent(const glm::vec3& translation, const glm::vec3& rotation, const glm::vec3& scale) : 
+        translation{translation}, rotation{rotation}, scale{scale} {}
+
+    glm::vec3 translation{0};
+    glm::vec3 rotation{0};
+    glm::vec3 scale{1};
+};
+
+class MeshComponent : ecs::Component<MeshComponent> {
+  public:
+    MeshComponent(const SharedPtr<Mesh>& mesh) : mesh{mesh} {}
+
+    SharedPtr<Mesh> mesh;
+};
