aabd4d6ccb396a3707d14798a77231909b548ffe
vssense <zelenkin.sa@phystech.edu>
2022-04-28 01:39:13 +0300

    add Keyboard and README to platform

--- src/main.cpp
+++ src/main.cpp
@@ -85,111 +85,66 @@ void mouseMoveCallback(GLFWwindow* window, double newX, double newY)
     glfwSetCursorPos(window, 0, 0);
 }
 
-// int main(int argc, const char* argv[])
-// {
-//     GLFWwindow* window = nullptr;
-//     if (!glfwInit())
-//     {
-//         std::cout << "Failed to initialize GLFW" << std::endl;
-//         return -1;
-//     }
-
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-// #ifdef __APPLE__
-//     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-// #endif
-
-//     window = glfwCreateWindow(640, 480, "Hello, OpenGL!", /*monitor=*/nullptr, /*share=*/nullptr);
-//     if (!window)
-//     {
-//         std::cout << "Failed to create window" << std::endl;
-//         glfwTerminate();
-//         return -1;
-//     }
-
-//     int32_t frameBufferWidth, frameBufferHeight;
-//     glfwGetFramebufferSize(window, &frameBufferWidth, &frameBufferHeight);
-
-//     glfwMakeContextCurrent(window);
-
-//     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-//     {
-//         std::cout << "Failed to initialize GLAD" << std::endl;
-//         glfwTerminate();
-//         return -1;
-//     }
-
-//     g_Scene.camera = CreateShared<Camera>(glm::vec3{10, 5, 10}, glm::vec3{-1, -0.5, -1});
-//     g_Scene.light = CreateShared<LightSource>(glm::vec3{10, 0, 15}, glm::vec3{1, 1, 0});
-//     g_Scene.meshes.push_back(CreateShared<MeshInstance>(glm::vec3{0, 0, 0}, ParseMeshObj("../res/meshes/skameiki.obj")));
-
-//     SharedPtr<Shader> shader = Shader::Create("../res/shaders/basic.shader");
-
-//     Renderer3D::Init();
-//     Renderer3D::SetViewport(Viewport{0, 0, static_cast<uint32_t>(frameBufferWidth),
-//                                            static_cast<uint32_t>(frameBufferHeight)});
-
-//     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-//     glfwSetKeyCallback(window, keyCallback);
-//     glfwSetCursorPosCallback(window, mouseMoveCallback);
-
-//     while (!glfwWindowShouldClose(window))
-//     {
-//         Renderer3D::RenderScene(g_Scene, shader);
-
-//         glfwSwapBuffers(window);
-//         glfwPollEvents();
-//     }
-
-//     glfwTerminate();
-
-//     return 0;
-// }
-
-
-#include "platform/window.hpp"
-#include "platform/event.hpp"
-
-#include <iostream>
-
-using namespace input;
-
-int main()
+int main(int argc, const char* argv[])
 {
-    Window window{};
-    EventQueue::SetWindow(&window);
-
-    Event event{};
-
-    bool running = true;
-
-    while (running)
+    GLFWwindow* window = nullptr;
+    if (!glfwInit())
     {
-        while (PollEvent(&event))
-        {
-            switch (event.GetType())
-            {
-            case kQuit:
-                running = false;
-                break;
-            
-            case kKey:
-                if (event.GetKey().action == kRelease)
-                {
-                    std::cout << "scancode = " << event.GetKey().scancode << '\n';
-                    std::cout << "key      = " << event.GetKey().key << '\n' << '\n';
-                }
-                break;
-            default:
-                break;
-            }
-        }
-
-        std::cout << Keyboard::Pressed(Keys::kQKey) << ' ';
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    window = glfwCreateWindow(640, 480, "Hello, OpenGL!", /*monitor=*/nullptr, /*share=*/nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    int32_t frameBufferWidth, frameBufferHeight;
+    glfwGetFramebufferSize(window, &frameBufferWidth, &frameBufferHeight);
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    g_Scene.camera = CreateShared<Camera>(glm::vec3{10, 5, 10}, glm::vec3{-1, -0.5, -1});
+    g_Scene.light = CreateShared<LightSource>(glm::vec3{10, 0, 15}, glm::vec3{1, 1, 0});
+    g_Scene.meshes.push_back(CreateShared<MeshInstance>(glm::vec3{0, 0, 0}, ParseMeshObj("../res/meshes/skameiki.obj")));
+
+    SharedPtr<Shader> shader = Shader::Create("../res/shaders/basic.shader");
+
+    Renderer3D::Init();
+    Renderer3D::SetViewport(Viewport{0, 0, static_cast<uint32_t>(frameBufferWidth),
+                                           static_cast<uint32_t>(frameBufferHeight)});
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetCursorPosCallback(window, mouseMoveCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        Renderer3D::RenderScene(g_Scene, shader);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
